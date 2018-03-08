7a3a27a8f3d6ffb97f1cceff87a950726a411f84
Niitneuk <35228403+Niitneuk@users.noreply.github.com>
2018-03-08 13:35:30 +0100

    Add files via upload

---
+++ INSERTINF.sql
@@ -0,0 +1,92 @@
+INSERT INTO infirmiere VALUES
+(
+    1,
+    'BELLUCI',
+    'NIKITA',
+	'nikita.belluci',
+	'8fTrDS?#'
+);
+
+INSERT INTO infirmiere VALUES
+(
+    2,
+    'GREY',
+    'SACHA',
+	'sacha.grey',
+	'64rHFBV'
+);
+
+INSERT INTO infirmiere VALUES
+(
+    3,
+    'WATSON',
+    'EMMA',
+	'emma.watson',
+	'GCJTviir*#'
+);
+
+INSERT INTO PATIENT VALUES
+(
+    1,
+	'FELER',
+    'LIVIO',
+    '21545555777865',
+	'MANQUE D AFFECTION / DEPRESSION CHRONIQUE',
+    '22/02/2017',
+	2
+);
+
+INSERT INTO PATIENT VALUES
+(
+    2,
+	'COCHET',
+    'MORGAN',
+    '11545555777865',
+	'GASTRO',
+    '02/08/2018',
+	3
+);
+
+INSERT INTO PATIENT VALUES
+(
+    3,
+	'SILVESTRE',
+    'CHRISTOPHE',
+    '143567855777865',
+	'HERPES TRES INFECTIEUX / RISQUE D AMPUTATION',
+    '07/03/2018',
+	3
+);
+
+INSERT INTO PATIENT VALUES
+(
+    4,
+	'MASSE',
+    'PIERRE',
+    '143589055777865',
+	'CHUTE EN MOTO',
+    '15/12/2017',
+	1
+);
+
+INSERT INTO PATIENT VALUES
+(
+    5,
+	'GUIBOUD',
+    'THEO',
+    '176894555777865',
+	'CANCER DE L ORTEIL',
+    '01/01/2017',
+	1
+);
+
+INSERT INTO PATIENT VALUES
+(
+    6,
+	'FOURNET',
+    'YANNIS',
+    '143543578777865',
+	'chikungunya',
+    '31/03/2222',
+	2
+);
