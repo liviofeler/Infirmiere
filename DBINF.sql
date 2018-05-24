f4c21e99f851f22dcc5a2603f82b3339bb0e17c1
liviofeler <35263119+liviofeler@users.noreply.github.com>
2018-05-24 15:50:59 +0200

    Update DBINF.sql

--- DBINF.sql
+++ DBINF.sql
@@ -2,19 +2,25 @@ CREATE TABLE infirmiere
 (
 	id INT PRIMARY KEY AUTO_INCREMENT,
 	idPatientACharge Int,
+	idUtilisateur Int,
 	nom VARCHAR(38),
-	prenom VARCHAR(38)
+	prenom VARCHAR(38),
+	FOREIGN KEY (idPatientACharge) REFERENCES patient(id),
+	FOREIGN KEY (idUtilisateur) REFERENCES utilisateur(id)
 );
 
 CREATE TABLE patient
 (
 	id INT PRIMARY KEY AUTO_INCREMENT ,
+	idUtilisateur,
 	nom VARCHAR(38),
     	prenom VARCHAR(38),
 	numSecu VARCHAR(15),
 	Pathologie VARCHAR(50),
 	dateRecenteVisite Date,
-	idInfirmiere INT
+	idInfirmiere INT,
+	FOREIGN KEY (idInfirmiere) REFERENCES infirmiere(id),
+	FOREIGN KEY (idUtilisateur) REFERENCES utilisateur(id)
 );
 
 CREATE TABLE utilisateur
